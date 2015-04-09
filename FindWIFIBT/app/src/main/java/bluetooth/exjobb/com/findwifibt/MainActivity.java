fc267e18bd55a3665ba11738f74638f6f5183e96
sebo1 <sebbe_olsson@live.se>
2015-04-09 15:14:04 +0200

    Code

--- FindWIFIBT/app/src/main/java/bluetooth/exjobb/com/findwifibt/MainActivity.java
+++ FindWIFIBT/app/src/main/java/bluetooth/exjobb/com/findwifibt/MainActivity.java
@@ -1,18 +1,59 @@
 package bluetooth.exjobb.com.findwifibt;
 
+import android.view.MenuItem;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+    private ArrayList<String> deviceList = new ArrayList<String>();
+    private final static int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(mBlueAdapter==null){
+
+        }
+
+        if(!mBlueAdapter.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
+
+
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                deviceList.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
+    // Register the BroadcastReceiver
+    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+    registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
