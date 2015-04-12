8f4a3da1dd82b5713d8ac6ea7f59b628cd2a2293
sebo1 <sebbe_olsson@live.se>
2015-04-12 16:28:57 +0200

    More code
    Custom made adapter works, the search button also. Better BT code
    implemented but not tested.

---
+++ FindWIFIBT/app/src/main/java/bluetooth/exjobb/com/findwifibt/Devices.java
@@ -0,0 +1,15 @@
+package bluetooth.exjobb.com.findwifibt;
+
+/**
+ * Created by sebastian olsson on 15-04-12.
+ */
+public class Devices {
+    public String macAddress;
+    public String type;
+
+    public Devices(String macAddress, String type){
+        this.macAddress=macAddress;
+        this.type=type;
+    }
+
+}
--- FindWIFIBT/app/src/main/java/bluetooth/exjobb/com/findwifibt/MainActivity.java
+++ FindWIFIBT/app/src/main/java/bluetooth/exjobb/com/findwifibt/MainActivity.java
@@ -1,5 +1,6 @@
 package bluetooth.exjobb.com.findwifibt;
 
+import android.app.Activity;
 import android.view.MenuItem;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -11,41 +12,66 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
-    private ArrayList<String> deviceList = new ArrayList<String>();
+public class MainActivity extends Activity {
+    private ArrayAdapter<String> deviceArray ;
     private final static int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter mBlueAdapter;
+    private Button searchButton;
+    private ListView deviceList ;
 
+    private ArrayList<Devices> arrayListDevices;
+    private DeviceAdapter deviceAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchButton = (Button)findViewById(R.id.search_button);
+        //mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        BluetoothAdapter mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
+        arrayListDevices = new ArrayList<Devices>();
+        deviceAdapter=new DeviceAdapter(this,arrayListDevices);
 
-        if(mBlueAdapter==null){
+        ListView listView = (ListView)findViewById(R.id.listViewDevices);
+        listView.setAdapter(deviceAdapter);
+        deviceAdapter.add(new Devices("Test1", "Test1.2"));
+        deviceAdapter.add(new Devices("Test2", "Test2.2"));
 
-        }
 
-        if(!mBlueAdapter.isEnabled()){
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
+/*        if(mBlueAdapter==null){
 
-        mBlueAdapter.startDiscovery();
+        }*/
 
-        // Register the BroadcastReceiver
+/*        // Register the BroadcastReceiver
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiver, filter); // Unregister in onDestroy
+        registerReceiver(mReceiver, filter); // Unregister in onDestroy*/
+
+                //search for bluetooth devices
+/*                if (!mBlueAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+                mBlueAdapter.startDiscovery();*/
+
+    }
+
+    public void scan(View view){
+        Toast.makeText(MainActivity.this, "Starting search for BT devices", Toast.LENGTH_SHORT).show();
+        deviceAdapter.add(new Devices("Test2", "Test2.2"));
     }
 
 
     // Create a BroadcastReceiver for ACTION_FOUND
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+/*    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             // When discovery finds a device
@@ -53,17 +79,18 @@ public class MainActivity extends ActionBarActivity {
                 // Get the BluetoothDevice object from the Intent
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 // Add the name and address to an array adapter to show in a ListView
-                deviceList.add(device.getName() + "\n" + device.getAddress());
+                //deviceAdapter.add( device.getName() + "\n" + device.getAddress()+"\n"+device.getBluetoothClass()); //needs api 18 device.getType()
+                deviceAdapter.add(new Devices(device.getName(),device.getAddress()));
+
             }
         }
-    };
+    };*/
 
-
-    @Override
+/*    @Override
     public void onDestroy() {
         unregisterReceiver(mReceiver);
 
-    }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
