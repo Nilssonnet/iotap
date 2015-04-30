e8e74e4dcd4227e10fd31b2465fcbaf2bb5bf5eb
Nilssonnet <nilssonnet@gmail.com>
2015-04-30 23:54:25 +0200

    Using 3 different php files to get value from db

--- FindWIFIBT/app/src/main/java/bluetooth/exjobb/com/findwifibt/DisplayFragment.java
+++ FindWIFIBT/app/src/main/java/bluetooth/exjobb/com/findwifibt/DisplayFragment.java
@@ -1,49 +1,23 @@
 package bluetooth.exjobb.com.findwifibt;
 
 
-import android.app.ProgressDialog;
-import android.content.Intent;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RadioGroup;
-import android.widget.SimpleAdapter;
 import android.widget.Toast;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 
@@ -52,7 +26,7 @@ import java.util.concurrent.ExecutionException;
  */
 public class DisplayFragment extends Fragment implements View.OnClickListener{
 
-    private String link = "http://213.65.109.112/get.php";
+    private String link;
     private String radioButtonResult = "";
 
     private ArrayList<Display> arrayListDisplay;
@@ -73,7 +47,6 @@ public class DisplayFragment extends Fragment implements View.OnClickListener{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         View view = inflater.inflate(R.layout.fragment_display, container, false);
 
         arrayListDisplay = new ArrayList<Display>();
@@ -91,12 +64,15 @@ public class DisplayFragment extends Fragment implements View.OnClickListener{
                 switch (checkedId) {
                     case R.id.radioButtonFullAnonDisplay:
                         radioButtonResult = "FullAnon";
+                        link = "http://213.65.109.112/getFullAnon.php";
                         break;
                     case R.id.radioButtonSemiAnonDisplay:
                         radioButtonResult = "SemiAnon";
+                        link = "http://213.65.109.112/getSemiAnon.php";
                         break;
                     case R.id.radioButtonNoAnonDisplay:
                         radioButtonResult = "NoAnon";
+                        link = "http://213.65.109.112/getNoAnon.php";
                         break;
                 }
             }
@@ -126,15 +102,17 @@ public class DisplayFragment extends Fragment implements View.OnClickListener{
                 String time[] = new String[result.size()];
                 String hash[] = new String[result.size()];
                 String device[] = new String[result.size()];
-                for (int i = 0; i < result.size(); i++){
-                    String[] parts = result.get(i).split(";");
-                    time[i] = parts[0];
-                    hash[i] = parts[1];
-                    device[i] = parts[2];
-                    j++;
-                }
-                for(int i = 0; i < j; i++){
-                    displayAdapter.add(new Display(time[i], hash[i], device[i]));
+                if(!result.contains("0 results")) {
+                    for (int i = 0; i < result.size(); i++) {
+                        String[] parts = result.get(i).split(";");
+                        time[i] = parts[0];
+                        hash[i] = parts[1];
+                        device[i] = parts[2];
+                        j++;
+                    }
+                    for (int i = 0; i < j; i++) {
+                        displayAdapter.add(new Display(time[i], hash[i], device[i]));
+                    }
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -142,25 +120,11 @@ public class DisplayFragment extends Fragment implements View.OnClickListener{
                 e.printStackTrace();
             }
         }
-
     }
 
     class GetAsyncTask extends AsyncTask<String, String, ArrayList<String>> {
         @Override
         protected ArrayList<String> doInBackground(String... arg0) {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost(link);
-            try {
-                ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
-                nameValuePairs.add(new BasicNameValuePair("selection", radioButtonResult));
-                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                HttpResponse response = httpclient.execute(httppost);
-            }
-            catch(Exception e)
-            {
-                Log.e("log_tag", "Error:  " + e.toString());
-            }
-
             ArrayList<String> devicesArray = new ArrayList<String>();
             int i = 0;
             URL urlObj = null;
